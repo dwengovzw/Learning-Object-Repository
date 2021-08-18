c1dc617368f734331d3b15e0023b345336b7b449
JariDeGraeve <degraevjari@live.be>
2021-08-18 11:17:25 +0200

    Added extra request for content with wrapper (image + styling)

--- app/controllers/api/learing_object_api_controller.js
+++ app/controllers/api/learing_object_api_controller.js
@@ -7,54 +7,25 @@ let logger = Logger.getLogger()
 
 let learningObjectApiController = {}
 
-
-learningObjectApiController.getContentObject = async (hruid, language, version) => {
-    let query = {
-        hruid: hruid,
-        language: language,
-        version: version,
-    };
-    let metadata;
+learningObjectApiController.getMetadata = async (query) => {
     let repos = new LearningObjectRepository();
+    let metadata;
     await new Promise((resolve) => {
         repos.find(query, (err, res) => {
             if (err) {
-                logger.error(`Could not retrieve learning object from database (with hruid: ${hruid}, language: ${language}, version: ${version}): ${err.message}`);
+                logger.error("Could not retrieve learning object from database with id " + req.params.id + ":" + err.message);
             }
             metadata = res[0];
             resolve();
         })
     });
-    let dirCont = fs.readdirSync(path.resolve(process.env.LEARNING_OBJECT_STORAGE_LOCATION, metadata._id.toString()));
-    let htmlFile = dirCont.find((file) => {
-        return file.match(/.*\.html/)
-    });
-    let filename = path.resolve(process.env.LEARNING_OBJECT_STORAGE_LOCATION, metadata._id.toString(), htmlFile);
-    let html;
-    try {
-        html = fs.readFileSync(filename, 'utf8')
-    } catch (err) {
-        console.error(err)
-    }
-    return html;
+    return metadata
 }
 
-learningObjectApiController.getLearningObject = async (req, res) => {
-    let query = req.query ? req.query : {};
-    let metadata;
-    let repos = new LearningObjectRepository();
 
-    await new Promise((resolve) => {
-        repos.find(query, (err, res) => {
-            if (err) {
-                logger.error("Could not retrieve learning object from database with query " + query + ":" + err.message);
-            }
-            metadata = res[0];
-            resolve();
-        })
-    });
+learningObjectApiController.getHtmlObject = async (query) => {
+    let metadata = await learningObjectApiController.getMetadata(query);
     let resHtml = "";
-
     if (metadata) {
         let id = metadata._id.toString();
         let file = path.resolve(process.env.LEARNING_OBJECT_STORAGE_LOCATION, id, "index.html");
@@ -67,7 +38,7 @@ learningObjectApiController.getLearningObject = async (req, res) => {
                 let regex = new RegExp("@@OBJECT_REPLACE\/(.*)\/(.*)\/(.*)@@", "g");
                 let match = regex.exec(resHtml)
                 while (match) {
-                    let objHtml = await learningObjectApiController.getContentObject(match[1], match[2], match[3]);
+                    let objHtml = await learningObjectApiController.getHtmlObject({ hruid: match[1], language: match[2], version: match[3] });
                     resHtml = resHtml.replace(`@@OBJECT_REPLACE/${match[1]}/${match[2]}/${match[3]}@@`, objHtml);
                     match = regex.exec(resHtml)
                 }
@@ -77,24 +48,29 @@ learningObjectApiController.getLearningObject = async (req, res) => {
             });
         })
     }
+    return resHtml;
+}
+
+learningObjectApiController.getLearningObject = async (req, res) => {
+    let query = req.query ? req.query : {};
+    let resHtml = await learningObjectApiController.getHtmlObject(query) || "";
     return res.send(resHtml);
 
 };
 
-learningObjectApiController.getMetadata = async (req, res) => {
+learningObjectApiController.getWrappedLearningObject = async (req, res) => {
     let query = req.query ? req.query : {};
-    let metadata;
-    let repos = new LearningObjectRepository();
-
-    await new Promise((resolve) => {
-        repos.find(query, (err, res) => {
-            if (err) {
-                logger.error("Could not retrieve learning object from database with id " + req.params.id + ":" + err.message);
-            }
-            metadata = res[0];
-            resolve();
-        })
+    let content = await learningObjectApiController.getHtmlObject(query) || "";
+    return res.render('api/learning_object/learning_object.getWrapped.ejs', {
+        content: content,
+        domainURL: process.env.DOMAIN_URL
     });
+}
+
+learningObjectApiController.requestMetadata = async (req, res) => {
+    let query = req.query ? req.query : {};
+    let metadata = await learningObjectApiController.getMetadata(query);
+
     if (metadata) {
         return res.json(metadata);
     }
--- app/routes/api/learing_object_api_router.js
+++ app/routes/api/learing_object_api_router.js
@@ -2,12 +2,16 @@ import express from "express"
 import learningObjectApiController from "../../controllers/api/learing_object_api_controller.js"
 let learningObjectApiRouter = express.Router({ mergeParams: true });
 
-learningObjectApiRouter.route("/getContent").get((req, res) => {
+learningObjectApiRouter.route("/getRaw").get((req, res) => {
     learningObjectApiController.getLearningObject(req, res);
 });
 
+learningObjectApiRouter.route("/getWrapped").get((req, res) => {
+    learningObjectApiController.getWrappedLearningObject(req, res);
+});
+
 learningObjectApiRouter.route("/getMetadata").get((req, res) => {
-    learningObjectApiController.getMetadata(req, res);
+    learningObjectApiController.requestMetadata(req, res);
 });
 
 export default learningObjectApiRouter;
