b2702fdbfa69cafbc7ff567b0172069a988dff34
Jari De Graeve <degraevejari@live.be>
2021-08-19 17:02:51 +0200

    changed way blockly scripts are loaded when blockly is embedded in md

--- app/processors/blockly/blockly_processor.js
+++ app/processors/blockly/blockly_processor.js
@@ -37,33 +37,16 @@ class BlocklyProcessor extends Processor {
 
         <script>
 
-            function loadScripts${args.id}() {
-                let dynamicScripts = [];
-                document.querySelectorAll('.dynamic_blockly_script').forEach((scr) => {
-                    dynamicScripts.push(scr.cloneNode(true))
-                    scr.remove();
-                })
-                loadScript${args.id}(0, dynamicScripts)
-            }
+            function loadDynamicScript${args.id}() {
 
-            function loadDynamicScript${args.id}(dynamicScripts) {
-                try{        
-                    // Hacky!
-                    while(!varDone){
-                        setTimeout(() => {  }, 500);
+                for(let i = 0; i < 14; i++) {
+                    let scr = document.getElementById("blockly_script" + i);
+                    if(!scr.dataset.loaded) {
+                        setTimeout(() => {  loadDynamicScript${args.id}(); }, 500);
                         return;
                     }
-                } catch(e){
-                    setTimeout(() => {  loadDynamicScript${args.id}(dynamicScripts); }, 500);
-                    return;
                 }
 
-                if(dynamicScripts){
-                    dynamicScripts.forEach((scr) => {
-                        document.getElementsByTagName("head")[0].appendChild(scr);
-                        console.log("loaded script " + scr.id);
-                    })
-                }
                 let code = 'function injectBlockly${args.id}() {let workspace = Blockly.inject("blocklyDiv_${args.id}", { readOnly: true, scrollbars: true, zoom: { controls: true, wheel: true, startScale: 1.0, maxScale: 3, minScale: 0.3, scaleSpeed: 1.2, pinch: true } }); var xml = Blockly.Xml.textToDom(\\'${blocklyXml}\\'); Blockly.Xml.domToWorkspace(xml, workspace); } injectBlockly${args.id}()'
                 let script = document.createElement("script");
                 script.className = "blockly_script dynamic_blockly_script";
@@ -74,7 +57,7 @@ class BlocklyProcessor extends Processor {
                 console.log("loaded blockly_script${args.id}");
             }
 
-            function loadScript${args.id}(index, dynamicScripts) {
+            function loadScript${args.id}(index) {
                 let scripts = ["blockly_compressed.js", "msg/${args.language}.js", "msg2/${args.language}.js", "blocks_compressed.js",
                 "blocks/arduino.js", "blocks/comments.js", "blocks/conveyor.js", "blocks/drawingrobot.js", "blocks/dwenguino_new.js",
                 "blocks/lists.js", "blocks/procedures.js", "blocks/socialrobot.js", "blocks/variables_dynamic.js", "blocks/variables.js"]
@@ -84,22 +67,26 @@ class BlocklyProcessor extends Processor {
                         let script = document.createElement("script");
                         script.id = "blockly_script" + index;
                         script.className = "blockly_script"
+                        script.dataset.loaded = false
                         script.type = "text/javascript";
                         script.src = "@@URL_REPLACE@@/static/js/" + scripts[index]
                         document.getElementsByTagName("head")[0].appendChild(script);
 
                         script.onload = function () {
                             console.log("loaded blockly_script" + index);
-                            loadScript${args.id}(index + 1, dynamicScripts);
+                            script.dataset.loaded = true
+                            loadScript${args.id}(index + 1);
                         }
                     } else{
-                        loadDynamicScript${args.id}(dynamicScripts);
+                        loadDynamicScript${args.id}();
+                        return;
                     }
                 } else {
-                    loadDynamicScript${args.id}(dynamicScripts);
+                    loadDynamicScript${args.id}();
+                    return
                 }
             }
-            loadScripts${args.id}();
+            loadScript${args.id}(0)
         </script>
         `
         return html; //TODO is not sanitized using DOMPurify.sanitize (problems with script tags)
