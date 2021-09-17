b3a55fd24b0fbf174e92dc75e33d8af94d2f170f
Tom Neutens <tomneutens@gmail.com>
2021-09-17 14:24:11 +0200

    updated server_deploy script

--- server_deploy.sh
+++ server_deploy.sh
@@ -17,6 +17,16 @@ echo "LEARNING_OBJECT_LOADING_SCHEDULE = '0 0 * * *'" >> .env # Each day at midn
 #echo "LEARNING_OBJECT_LOADING_SCHEDULE = '*/10 * * * * *'" >> .env
 echo "LEARNING_OBJECT_REPOSITORY_LOCATION = '/home/ubuntu/dwengo-deploy/learning_object_backend/repos'" >> .env
 
+# TODO update this for use with i-learn
+echo "I_LEARN_AUTHENTICATION_ENDPOINT = 'https://saltire.lti.app/platform/auth'" >> .env
+#"https://<i-learn-auth-provider-domain>/authorize"
+echo "I_LEARN_KEY_LOCATION = 'https://saltire.lti.app/platform/jwks'" >> .env
+#"https://<i-learn-auth-provider-domein>/.well-known/jwks"
+echo "I_LEARN_DWENGO_CLIENT_ID = '8a6ff54b-d850-43f5-9214-785b5cb19b3c'" >> .env
+echo "I_LEARN_ISSUER_ID = 'https://saltire.lti.app/platform/token/s65d9e32638de5de994a21990e481b776'" >> .env
+echo "I_LEARN_TOOL_LAUNCH_URI = 'https://83.217.67.53/backend'" >> .env
+echo "I_LEARN_REDIRECT_URI = 'https://83.217.67.53/backend/api/learningObject/getWrapped'" >> .env
+
 #restart application (was started with: pm2 start 'npm run start-production' --name learning-object-repository)
 
 pm2 restart learning-object-repository
