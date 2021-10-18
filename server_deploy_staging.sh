ac1460dbc4f835905d68f64c698ae8b5ba690292
Tom Neutens <tomneutens@gmail.com>
2021-10-18 16:05:45 +0200

    added deploy script for staging server

---
+++ server_deploy_staging.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+git pull origin main # Get the latest version of the code
+
+# Write the correct server environment settings to the .env file
+echo "NODE_ENV = production" > .env 
+echo "HTTP_PORT = 8085" >> .env
+echo "HTTPS_PORT = 44300" >> .env
+echo "KEY_FILE = './certs/key.pem'" >> .env
+echo "CERT_FILE = './certs/cert.pem'" >> .env
+echo "CA_FILE = ''" >> .env
+echo 'LEARNING_OBJECT_STORAGE_LOCATION = "storage"' >> .env
+echo 'DATABASE_URL=mongodb://localhost:27017/learning_object_database' >> .env
+echo 'ERASE_DATABASE_ON_SYNC=true' >> .env
+echo 'LEARNING_OBJECTS_GIT_REPOSITORY=https://github.com/dwengovzw/learning_content' >> .env
+echo 'DOMAIN_URL=https://83.217.69.213//backend' >> .env
+echo "LEARNING_OBJECT_LOADING_SCHEDULE = '0 0 * * *'" >> .env # Each day at midnight
+echo "LEARNING_OBJECT_REPOSITORY_LOCATION = '/home/ubuntu/dwengo-deploy/learning_object_backend/repos'" >> .env
+
+# TODO update this for use with i-learn
+echo "I_LEARN_AUTHENTICATION_ENDPOINT = 'https://auth-test.i-learn.be/authorize'" >> .env
+echo "I_LEARN_KEY_LOCATION = 'https://auth-test.i-learn.be/.well-known/jwks.json'" >> .env
+echo "I_LEARN_DWENGO_CLIENT_ID = 'yK0RF6IkVshhZjMIr1eTv3DfEWENH2NC'" >> .env
+echo "I_LEARN_ISSUER_ID = 'https://auth-test.i-learn.be'" >> .env
+echo "I_LEARN_TOOL_LAUNCH_URI = 'https://83.217.69.213/'" >> .env
+echo "I_LEARN_REDIRECT_URI = 'https://83.217.69.213//backend/api/learningObject/getWrapped'" >> .env
+echo "ADMIN_USER_USERNAME=dwengo" >> .env
+echo "ADMIN_USER_PASSWORD='$1'" >> .env  # Admin password is supplied at deploy time
+
+
+# Install dependancies
+npm install
+
+#restart application (was started with: pm2 start 'npm run start-production' --name learning-object-repository)
+
+pm2 restart learning-object-repository || pm2 start 'npm run start-production' --name learning-object-repository
