cb1b94444ac3f31f265b52594023d206f776d62f
Tom Neutens <tomneutens@gmail.com>
2021-10-18 20:40:20 +0200

    updateds deploy scripts to include hostnames

--- server_deploy_staging.sh
+++ server_deploy_staging.sh
@@ -12,7 +12,7 @@ echo 'LEARNING_OBJECT_STORAGE_LOCATION = "storage"' >> .env
 echo 'DATABASE_URL=mongodb://localhost:27017/learning_object_database' >> .env
 echo 'ERASE_DATABASE_ON_SYNC=true' >> .env
 echo 'LEARNING_OBJECTS_GIT_REPOSITORY=https://github.com/dwengovzw/learning_content' >> .env
-echo 'DOMAIN_URL=https://83.217.69.213//backend' >> .env
+echo 'DOMAIN_URL=https://staging.dwengo.org/backend' >> .env
 echo "LEARNING_OBJECT_LOADING_SCHEDULE = '0 0 * * *'" >> .env # Each day at midnight
 echo "LEARNING_OBJECT_REPOSITORY_LOCATION = '/home/ubuntu/dwengo-deploy/learning_object_backend/repos'" >> .env
 
@@ -21,8 +21,8 @@ echo "I_LEARN_AUTHENTICATION_ENDPOINT = 'https://auth-test.i-learn.be/authorize'
 echo "I_LEARN_KEY_LOCATION = 'https://auth-test.i-learn.be/.well-known/jwks.json'" >> .env
 echo "I_LEARN_DWENGO_CLIENT_ID = 'yK0RF6IkVshhZjMIr1eTv3DfEWENH2NC'" >> .env
 echo "I_LEARN_ISSUER_ID = 'https://auth-test.i-learn.be'" >> .env
-echo "I_LEARN_TOOL_LAUNCH_URI = 'https://83.217.69.213/'" >> .env
-echo "I_LEARN_REDIRECT_URI = 'https://83.217.69.213//backend/api/learningObject/getWrapped'" >> .env
+echo "I_LEARN_TOOL_LAUNCH_URI = 'https://staging.dwengo.org/'" >> .env
+echo "I_LEARN_REDIRECT_URI = 'https://staging.dwengo.org/backend/api/learningObject/getWrapped'" >> .env
 echo "ADMIN_USER_USERNAME=dwengo" >> .env
 echo "ADMIN_USER_PASSWORD='$1'" >> .env  # Admin password is supplied at deploy time
 
